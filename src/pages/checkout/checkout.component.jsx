94f5966850434305f2ecc5279fc45c11c8951971
Joseph Marin <jmarin@tripadvisor.com>
2020-03-24 19:29:59 -0400

    Added strip component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({cartItems, total}) => (<div className='checkout-page'>
   <div className='checkout-header'>
@@ -25,6 +26,14 @@ const CheckoutPage = ({cartItems, total}) => (<div className='checkout-page'>
   </div>
   {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
   <div className='total'>TOTAL: ${total}</div>
+
+  <div className='test-warning'>
+    *Please use the following test credit card for payments*
+    <br/>
+    4242 4242 4242 4242 - Exp: any future date - CVV: 123
+  </div>
+
+  <StripeCheckoutButton price={total} />
 </div>);
 
 const mapStateToProps = createStructuredSelector({cartItems: selectCartItems, total: selectCartTotal});
