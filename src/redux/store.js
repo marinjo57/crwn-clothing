e039e378668a8106c48de1577c98c75bff9ac18e
Joseph Marin <jmarin@tripadvisor.com>
2020-05-05 14:53:16 -0400

    updates to crwn-database

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,20 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+import {fetchCollectionsStart} from './shop/shop.sagas'
 
-const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'developmen') {
   middlewares.push(logger);
 };
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart)
 
 export const persistor = persistStore(store);
 
